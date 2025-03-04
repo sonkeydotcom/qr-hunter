b55b7820681fcb46cf90a2cf501c93d25d210610
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-04 15:44:58 +0000

    Mar 4, 2025, 4:44 PM

--- components/Background.tsx
+++ components/Background.tsx
@@ -9,6 +9,15 @@ const Background = ({ children }: BackgroundProps) => {
   return <View style={styles.container}>{children}</View>;
 };
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "red",
+    zIndex: -1, // Ensure the background is behind all other elements.
+    flex: 1,
+  },
 });
 export default Background;
