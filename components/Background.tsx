e1919f82f963b3480115bfea8d54f8328a21560f
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-04 16:15:43 +0000

    Mar 4, 2025, 5:15 PM

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,6 +1,6 @@
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { StyleSheet, ViewStyle } from "react-native";
+import { StyleSheet, View, ViewStyle } from "react-native";
 
 type BackgroundProps = {
   children: React.ReactNode;
@@ -9,18 +9,32 @@ type BackgroundProps = {
 
 const Background = ({ children, style }: BackgroundProps) => {
   return (
-    <LinearGradient
-      colors={["#F5F5F5", "#E0E0E0"]} // Customize your gradient colors
-      style={styles.container}
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "orange",
+      }}
     >
-      {children}
-    </LinearGradient>
+      <LinearGradient
+        colors={["#F5F5F5", "#E0E0E0"]} // Customize your gradient colors
+        style={styles.container}
+      >
+        {children}
+      </LinearGradient>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    // height: "100%",
   },
 });
 export default Background;
