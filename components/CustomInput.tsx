7d6d940778ccc34caa4ea0da45f54c7b48fe7209
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-04 16:25:39 +0000

    Mar 4, 2025, 5:25 PM

---
+++ components/CustomInput.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+
+const CustomInput = () => {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+};
+
+export default CustomInput;
+
+const styles = StyleSheet.create({});
