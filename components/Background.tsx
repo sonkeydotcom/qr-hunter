21887589bb89cf8b5abb55d326efdff8c839bdaf
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-04 15:38:58 +0000

    Mar 4, 2025, 4:38 PM

---
+++ components/Background.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+type BackgroundProps = {
+  children: React.ReactNode;
+};
+
+const Background = ({ children }: BackgroundProps) => {
+  return <View>{children}</View>;
+};
+
+export default Background;
+
+const styles = StyleSheet.create({});
