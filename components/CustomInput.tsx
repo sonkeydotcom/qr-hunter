3d4113c2a3cec95583e300c1b827f605fdba5622
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-04 16:40:47 +0000

    Mar 4, 2025, 5:40 PM

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,14 +1,71 @@
-import React from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import {
+  StyleProp,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
 
-const CustomInput = () => {
+interface CustomInputProps extends TextInput {
+  title: string;
+  containerStyle?: ViewStyle;
+  inputStyle?: StyleProp<TextInput>;
+  secureTextEntry?: boolean;
+  value: string;
+  placeholder?: string;
+  onChangeText: () => void;
+}
+
+const CustomInput: React.FC<CustomInputProps> = ({
+  title,
+  placeholder,
+  onChangeText,
+  containerStyle,
+  value,
+  ...props
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [secureTextEntry, setSecureTextEntry] = useState(title === "password");
   return (
-    <View>
-      <TextInput />
+    <View
+      style={[
+        styles.container,
+        { borderColor: isFocused ? "blue" : "#cccccc" },
+        containerStyle,
+      ]}
+    >
+      <TextInput
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor={"#f5f5f5"}
+        style={styles.input}
+        secureTextEntry={secureTextEntry}
+        {...props}
+      />
+      {title === "password" && (
+        <TouchableOpacity
+          onPress={() => setSecureTextEntry(!secureTextEntry)}
+          style={styles.eyeIcon}
+        >
+          <Ionicons
+            name={secureTextEntry ? "eye" : "eye-off"}
+            size={20}
+            color="black"
+          />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
 
 export default CustomInput;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  eyeIcon: {},
+  input: {},
+});
